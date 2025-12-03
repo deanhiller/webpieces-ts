fdf992d9ce90a282526840b013a47ac852fa5687
Dean Hiller <dean@biltup.com>
2025-12-03 16:27:04 +0200

    lines too long

--- packages/tooling/dev-config/eslint-plugin/index.ts
+++ packages/tooling/dev-config/eslint-plugin/index.ts
@@ -6,9 +6,13 @@
  */
 
 import catchErrorPattern from './rules/catch-error-pattern';
+import maxMethodLines from './rules/max-method-lines';
+import maxFileLines from './rules/max-file-lines';
 
 export = {
     rules: {
         'catch-error-pattern': catchErrorPattern,
+        'max-method-lines': maxMethodLines,
+        'max-file-lines': maxFileLines,
     },
 };
